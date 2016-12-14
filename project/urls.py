43856c0e7863387bd33f304d8036f12fd0368f0f
Gary Bullock <g.bullock91@gmail.com>
2016-12-14 23:12:10 +0000

    urls fix

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('home.urls')),
-    url(r'^stories/$', include('story.urls'))
+    url(r'', include('home.urls')),
+    url(r'^stories/', include('story.urls'))
 ]
